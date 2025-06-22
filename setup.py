2b2d1f36cfa64c2b2561ffd86a034fa475cdecea
WeslenPy <weslenjhony@gmail.com>
2025-06-22 11:51:47 -0400

    fix

--- setup.py
+++ setup.py
@@ -1,41 +1,28 @@
-from setuptools import setup, Extension
+from __future__ import print_function
 from glob import glob
+from setuptools import setup,Extension
 
-sources = [
-    "curve25519module.c",
-    "curve/curve25519-donna.c",
-    *glob("curve/ed25519/*.c"),
-    *glob("curve/ed25519/additions/*.c"),
-    *glob("curve/ed25519/nacl_sha512/*.c"),
-]
-
-include_dirs = [
-    "curve/ed25519/nacl_includes",
-    "curve/ed25519/additions",
-    "curve/ed25519"
-]
-
-compile_args = ["-march=native", "-O3", "-msse", "-msse2", "-mfma", "-mfpmath=sse"]
-
-extensions = [
-    Extension(
-        name="axolotl_curve25519",
-        sources=sources,
-        include_dirs=include_dirs,
-        extra_compile_args=compile_args,
-        libraries=["m"],
-    )
-]
-
+sources = ['curve25519module.c', 'curve/curve25519-donna.c']
+sources.extend(glob("curve/ed25519/*.c"))
+sources.extend(glob("curve/ed25519/additions/*.c"))
+sources.extend(glob("curve/ed25519/nacl_sha512/*.c"))
+#headers = ['curve25519-donna.h']
+module_curve = Extension('axolotl_curve25519',
+                    sources = sorted(sources),
+#                   headers = headers,
+                    include_dirs = [
+                      'curve/ed25519/nacl_includes',
+                      'curve/ed25519/additions',
+                      'curve/ed25519'
+                      ]
+                    )
 setup(
-    name="python-axolotl-curve25519",
-    version="1.0.2",
-    author="Tarek Galal",
-    author_email="tare2.galal@gmail.com",
-    description="curve25519 with ed25519 signatures, used by libaxolotl",
-    license="GPLv3",
-    install_requires=[
-        "setuptools>=71.0.4"
-    ],
-    ext_modules = [extensions],
+    name='python-axolotl-curve25519',
+    version="0.4.1-2",
+    license='GPLv3 License',
+    author='Tarek Galal',
+    ext_modules = [module_curve],
+    author_email='tare2.galal@gmail.com',
+    description='curve25519 with ed25519 signatures, used by libaxolotl',
+    platforms='any'
 )
